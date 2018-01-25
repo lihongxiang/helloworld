9bba1ce3ce8968470a70d8efc4fe2941446c5372
lihongxiang <lhxsir@outlook.com>
2018-01-25 17:38:52 +0800

    Initial commit
    test1

---
+++ HelloWorld1.scala
@@ -0,0 +1,14 @@
+package com.egridcloud.spark
+
+/**
+  * Created by LHX on 2018/1/11 0011 下午 6:58.
+  */
+object HelloWorld {
+  def main(args: Array[String]): Unit = {
+    println("hello")
+    val str="20171_aa"
+    val str2="201712_aa"
+    println(str.startsWith("201712_"))
+    println(str2.startsWith("201712_"))
+  }
+}
